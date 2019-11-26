571e1ad9525c6e9d6e15464cc69eb7d052637c67
tawny francis <tawnyfrancis@gmail.com>
2019-11-25 21:10:31 -0500

    installed node packages and updated code for burger functions

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name) VALUES ('turkey burger');
+INSERT INTO burgers (name) VALUES ('quarter pounder');
+INSERT INTO burgers (name, devoured) VALUES ('veggie burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('bacon bbq burger', true);
+
